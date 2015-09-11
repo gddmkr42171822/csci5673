7f4a0ba32ab7e54833cf84a0f13f004b9302fbf1
Bob <Bob@10.0.0.5>
2015-09-10 21:52:07 -0600

    UDP client and server send a message between eachother.  Still need to record local times of receiving and sending on client.

--- UDP_client_server/Client.py
+++ UDP_client_server/Client.py
@@ -2,7 +2,58 @@
 Created on Sep 10, 2015
 
 @author: Bob
+
+Sources
+-------
+https://pymotw.com/2/socket/udp.html - UDP echo server and client code
+
+Pseudocode
+-----------
+
+Part 1: Establish Client on machine
+
+Figure out ip of server and its port
+Create UDP socket
+
+Part 2: Wait for messages and echo them back
+Infinite while loop to wait
+Receive the data and then resend it
+Repeat
+
+Part 3: Record local clock times when sending and receiving (maybe output them to a file?)
+
+Part 4: TODO handle exceptions
+
+
 '''
+import socket
+
+def main():
+    serverIP = '0.0.0.0'
+    serverPort = 56513
+    
+    #create the UDP socket
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    
+    #message to be sent to server
+    message = 'Hello World!'
+    
+    try: 
+        #send the message
+        print "Sending {0} to {1} on port {2}.".format(message, serverIP, serverPort)
+        sock.sendto(message, (serverIP, serverPort))
+        
+        #wait for echo from server
+        print "Waiting for echo from server."
+        data, serverAddr = sock.recvfrom(1024)
+        print "Received {0} bytes from {1}".format(len(data), serverAddr)
+        print "Message received: {0}".format(data)
+    finally:
+        sock.close()
+        print "Closed client socket."
+        
+    
+    
 
 if __name__ == '__main__':
-    pass
+    main()
--- UDP_client_server/Server.py
+++ UDP_client_server/Server.py
@@ -2,7 +2,50 @@
 Created on Sep 10, 2015
 
 @author: Bob
-'''
 
+Sources
+-------
+https://pymotw.com/2/socket/udp.html - UDP echo server and client code
+
+Pseudocode
+-----------
+
+Part 1: Establish Sever on IP address and port
+Create UDP socket
+Bind Server to ip address and port
+
+Part 2: Wait for messages and echo them back
+Infinite while loop to wait
+Receive the data and then resend it
+Repeat
+
+Part 3: TODO handle exceptions
+
+'''
+import socket
+
+def main():
+    #Create the UDP socket
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    
+    #Bind the server to localhost and a port between 1024 and 65535
+    sock.bind(('', 0))
+    print "Bound server to address {0} and port {1}".format(sock.getsockname()[0], sock.getsockname()[1])
+    
+    #wait for messages to come it
+    #receive a message up to 1024 bytes 
+    try:
+        data, addr = sock.recvfrom(1024)
+        print "Received {0} bytes from {1}".format(len(data), addr)
+        print "Message received: {0}".format(data)
+    
+        #send the message back to the client
+        if data:
+            sent = sock.sendto(data, addr)
+            print "Sent {0} bytes back to {1}.".format(sent, addr)
+    finally:
+        sock.close()
+        print "Closed server socket."
+        
 if __name__ == '__main__':
-    pass
+    main()
