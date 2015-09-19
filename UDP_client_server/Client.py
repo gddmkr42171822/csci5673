f29d9e95c39b0ce1eaf5d0065cf41c6fb306dbbd
Bob <Bob@10.0.0.5>
2015-09-18 21:01:14 -0600

    Ran the scenarios and got all of the times message were sent and received at.

--- UDP_client_server/Client.py
+++ UDP_client_server/Client.py
@@ -16,9 +16,8 @@ Figure out ip of server and its port
 Create UDP socket
 
 Part 2: Wait for messages and echo them back
-Infinite while loop to wait
 Receive the data and then resend it
-Repeat
+End the server program and close the socket
 
 Part 3: Record local clock times when sending and receiving (maybe output them to a file?)
 
@@ -27,31 +26,59 @@ Part 4: TODO handle exceptions
 
 '''
 import socket
+import datetime
 
 def main():
-    serverIP = '0.0.0.0'
-    serverPort = 56513
+    
+    #Declare empty strings for storing the time a message is sent and received
+    timeReceived = ""
+    timeSent = ""
+    
+    sendTimes = []
+    receiveTimes = []
+    
+    serverIP = '128.138.201.67'
+    serverPort = 46805
+    #message to be sent to server
+    message = 'Hello World!'
     
     #create the UDP socket
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     
-    #message to be sent to server
-    message = 'Hello World!'
-    
-    try: 
-        #send the message
-        print "Sending {0} to {1} on port {2}.".format(message, serverIP, serverPort)
-        sock.sendto(message, (serverIP, serverPort))
+    try:
         
-        #wait for echo from server
-        print "Waiting for echo from server."
-        data, serverAddr = sock.recvfrom(1024)
-        print "Received {0} bytes from {1}".format(len(data), serverAddr)
-        print "Message received: {0}".format(data)
+        #Repeat the send and receive five times
+        for i in range(0, 5):
+            #send the message
+            print "Sending {0} to {1} on port {2}.".format(message, serverIP, serverPort)
+            sock.sendto(message, (serverIP, serverPort))
+            timeSent = datetime.datetime.now().time().strftime("%H:%M:%S.%f")
+            print "Client send message at time: " + timeSent
+            
+            #wait for echo from server
+            print "Waiting for echo from server."
+            data, serverAddr = sock.recvfrom(1024)
+            timeReceived = datetime.datetime.now().time().strftime("%H:%M:%S.%f")
+            print "Client received message at time: " + timeReceived
+            print "Received {0} bytes from {1}".format(len(data), serverAddr)
+            print "Message received: {0}".format(data)
+            
+            #Keep a list of the times a message is sent an received
+            sendTimes.append(timeSent)
+            receiveTimes.append(timeReceived)
+        
     finally:
         sock.close()
         print "Closed client socket."
         
+    outputFilePath = "client_times_diff_wan.txt"
+    print "Writing times to file: " + outputFilePath
+    with open(outputFilePath, "w") as f:
+        f.write("Scenario: Client and Server are on different machines accross WAN.\n")
+        for time in sendTimes:
+            f.write("Send time: " + time + "\n")
+        for time in receiveTimes:
+            f.write("Receive time: " + time + "\n")
     
     
 
--- UDP_client_server/Server.py
+++ UDP_client_server/Server.py
@@ -23,29 +23,58 @@ Part 3: TODO handle exceptions
 
 '''
 import socket
+import datetime
 
 def main():
+    
+    #Declare empty strings for storing the time a message is sent and received
+    timeReceived = ""
+    timeSent = ""
+    
+    sendTimes = []
+    receiveTimes = []
+    
     #Create the UDP socket
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     
     #Bind the server to localhost and a port between 1024 and 65535
     sock.bind(('', 0))
-    print "Bound server to address {0} and port {1}".format(sock.getsockname()[0], sock.getsockname()[1])
+    print "Bound server to address {0} and port {1}".format(socket.gethostbyname(socket.gethostname()), sock.getsockname()[1])
     
-    #wait for messages to come it
-    #receive a message up to 1024 bytes 
     try:
-        data, addr = sock.recvfrom(1024)
-        print "Received {0} bytes from {1}".format(len(data), addr)
-        print "Message received: {0}".format(data)
-    
-        #send the message back to the client
-        if data:
-            sent = sock.sendto(data, addr)
-            print "Sent {0} bytes back to {1}.".format(sent, addr)
+        
+        #Repeat the send and receive five times
+        for i in range(0, 5):
+            #wait for messages to come it
+            #receive a message up to 1024 bytes 
+            data, addr = sock.recvfrom(1024)
+            timeReceived = datetime.datetime.now().time().strftime("%H:%M:%S.%f")
+            print "Server received message at time: " + timeReceived
+            print "Received {0} bytes from {1}".format(len(data), addr)
+            print "Message received: {0}".format(data)
+        
+            #send the message back to the client
+            if data:
+                sent = sock.sendto(data, addr)
+                timeSent = datetime.datetime.now().time().strftime("%H:%M:%S.%f")
+                print "Server sent message at time: " + timeSent
+                print "Sent {0} bytes back to {1}.".format(sent, addr)
+            
+            sendTimes.append(timeSent)
+            receiveTimes.append(timeReceived)
+            
     finally:
         sock.close()
         print "Closed server socket."
+    
+    outputFilePath = "server_times_same_machine.txt"
+    print "Writing times to the file: " + outputFilePath    
+    with open(outputFilePath, "w") as f:
+        f.write("Scenario: Client and Server are the same machine.\n")
+        for time in sendTimes:
+            f.write("Send time: " + time + "\n")
+        for time in receiveTimes:
+            f.write("Receive time: " + time + "\n")
         
 if __name__ == '__main__':
     main()
